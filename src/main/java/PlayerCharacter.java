f73d467f99b323625e4f58265f318979196d0306
learnjavabh <learnmsbh@outlook.de>
2025-03-27 15:43:19 +0100

    added getX(), getY() and move()

--- src/main/java/PlayerCharacter.java
+++ src/main/java/PlayerCharacter.java
@@ -2,4 +2,16 @@ public class PlayerCharacter {
     public static int getX() {
         return 0;
     }
+
+    public static int getY() {
+        return 0;
+    }
+
+    public static int[] move(String w) {
+        int[] pos = {0,0};
+        if (w.equals("W")) {
+            pos = new int[] {0,1};
+        }
+        return pos;
+    }
 }
